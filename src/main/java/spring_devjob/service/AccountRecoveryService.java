f3c11be99ed260c998fd5bbdb2346df7e4952ec9
lean270804@gmail.com <lean270804@gmail.com>
2025-04-16 14:40:37 +0700

    dev

--- src/main/java/spring_devjob/service/AccountRecoveryService.java
+++ src/main/java/spring_devjob/service/AccountRecoveryService.java
@@ -3,18 +3,19 @@ package spring_devjob.service;
 import com.nimbusds.jose.JOSEException;
 import spring_devjob.dto.request.EmailRequest;
 import spring_devjob.dto.request.ResetPasswordRequest;
+import spring_devjob.dto.response.VerificationCodeResponse;
 import spring_devjob.entity.ForgotPasswordToken;
-import spring_devjob.entity.VerificationCodeEntity;
+import spring_devjob.entity.RedisVerificationCode;
 
 public interface AccountRecoveryService {
 
-    VerificationCodeEntity forgotPassword(EmailRequest request);
+    VerificationCodeResponse forgotPassword(EmailRequest request);
 
     ForgotPasswordToken verifyForgotPasswordCode(String email, String verificationCode) throws JOSEException;
 
     void resetPassword(ResetPasswordRequest request);
 
-    VerificationCodeEntity recoverAccount(String email);
+    VerificationCodeResponse recoverAccount(String email);
 
     void verifyRecoverAccountCode(String email, String verificationCode);
 }
